55343402c67f3d972273dcff1613efd036608433
yangjunchao888 <625977644@qq.com>
2022-01-25 17:58:58 +0800

    update sample

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# goLog
+func main() {
+    goLog.SetSysLevel(goLog.DebugLevel)
+    goLog.InitConf("E:\\qnzs\\aiqc\\src\\goLog\\logeeee", 0, 0, false)
+    goLog.SetupQnFormatByDate("E:\\qnzs\\aiqc\\src\\goLog\\logsss", "qnApi.log", "demoService", 0, 0, true, goLog.DebugLevel)
+    goLog.LogPrintfWithID(goLog.DebugLevel, "xxx", "xxx %s", "ddd")
+    goLog.LogPrintf(goLog.DebugLevel,"xxx %s", "ddd")
+    goLog.GetQnFileLogger().ParseQnApiLogFormat(true, 0, []string{"12345", "aaaaa", "-----"})
+    goLog.GetQnFileLogger().ParseQnApiLogFormat(false, 100, "success")
+    goLog.GetFileLogger("demo.log").Errorf("this is %s", "demo log")
+    goLog.GetFileLogger("success.log").Errorf("this is %s", "success log")
+}
--- sample/sample.go
+++ sample/sample.go
@@ -4,16 +4,16 @@ import "github.com/JuncYoung/goLog"
 
 // example
 func main() {
-	goLog.SetSysLevel(0)
-	goLog.InitConf("E:\\qnzs\\goPro\\goLog\\logeeee", 7, 0, false)
-	goLog.SetupQnFormatByDate("E:\\qnzs\\goPro\\goLog\\logsss", "qnApi.log", "demoService", 7, 0, true, goLog.DebugLevel)
+	goLog.SetSysLevel(goLog.DebugLevel)
+	goLog.InitConf("E:\\qnzs\\aiqc\\src\\goLog\\logeeee", 0, 0, false)
+	goLog.SetupQnFormatByDate("E:\\qnzs\\aiqc\\src\\goLog\\logsss", "qnApi.log", "demoService", 0, 0, true, goLog.DebugLevel)
 
-
-	goLog.LogPrintfWithID(goLog.DebugLevel, "xxx", "ssssssssssssss %s", "ddd")
-	goLog.LogPrintf(goLog.DebugLevel,"ssssssssssssss %s", "ddd")
+	goLog.LogPrintfWithID(goLog.DebugLevel, "xxx", "xxx %s", "ddd")
+	goLog.LogPrintf(goLog.DebugLevel,"xxx %s", "ddd")
 
 	goLog.GetQnFileLogger().ParseQnApiLogFormat(true, 0, []string{"12345", "aaaaa", "-----"})
 	goLog.GetQnFileLogger().ParseQnApiLogFormat(false, 100, "success")
 
-	goLog.GetFileLogger("demo.log").Errorf("fffffff %s", "good")
+	goLog.GetFileLogger("demo.log").Errorf("this is %s", "demo log")
+	goLog.GetFileLogger("success.log").Errorf("this is %s", "success log")
 }
